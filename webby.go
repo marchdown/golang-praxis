ad0b50e5bea6c92c057e193f68698dda8fd1a550
marchdown <marchdown@gmail.com>
2011-07-01 05:09:58 +0100

    Tweaked webby a bit

--- webby.go
+++ webby.go
@@ -3,13 +3,22 @@ package main
 import (
 	"fmt"
 	"http"
+	"os"
+	"flag"
 )
+
+var port = flag.String("port", "2048", "TCP port for inbound connections")
+var quiet = flag.Bool("q", false, "Be quiet, don't print anything")
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
 
 func main() {
+	flag.Parse()
 	http.HandleFunc("/", handler)
-	http.ListenAndServe(":2048", nil)
+	if !*quiet {
+		os.Stdout.WriteString("Listening on port " + *port + "\n")
+	}
+	http.ListenAndServe(":" + *port, nil)
 }
